3827c22f3dc0baf835f11315cd39f57337bb61b7
Alireza Kheirkhahan <kheirkhahan@gmail.com>
2015-09-28 17:22:35 -0500

    Adding AGAS registration

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+
 #include <hpxio/local_file.hpp>
 
 #include <hpx/hpx_init.hpp>
@@ -14,12 +15,39 @@
 int hpx_main()
 {
 	{
+		//defining a type for hpxio local file component, aka server.
 		typedef hpx::io::server::local_file file_type;
 
+		// creating a server and client at the same time
+		// the constructor for client need the gid of component.
+		// new_ returns the gid of component.
 		hpx::io::local_file lf(
 			hpx::components::new_<file_type>(hpx::find_here()));
 
-		std::cout << "component is created." << std::endl;
+		// this is the output file
+		std::string filename = "output";
+
+		// we open the file and write a small message
+		lf.open_sync(filename, "w");
+		std::string message = "Hellow World from the parrent process!\n";
+		lf.write_sync(std::vector<char> (message.begin(), message.end()));
+
+		// register the filename through AGAS.
+		hpx::register_with_basename(filename, lf.get_gid(), 0);
+
+		/////////////////////////////////////////
+		//This section suppose to run in another process
+		// we have the file and querry the AGAS.
+		// And create new client with gid
+		hpx::io::local_file lf2(
+			hpx::find_from_basename(filename, 0).get());
+		std::string message2 = "Hellow World from the child process!\n";
+		lf2.write_sync(std::vector<char> (message2.begin(), message2.end()));
+
+
+		//somebody should close the file or not!
+		lf2.close_sync();
+
 	}
 	return hpx::finalize();
 }
